b4a9f02db5cdb4c9ae426c26936a9010dc3b0b80
Mefodei <mefodei.link@gmail.com>
2023-05-10 20:47:50 +0400

    - update unity tool

--- LeoEcs.Shared/Systems/InstantDestroySystem.cs
+++ LeoEcs.Shared/Systems/InstantDestroySystem.cs
@@ -4,15 +4,19 @@
     using Components;
     using Leopotam.EcsLite;
 
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+#endif
+    
+#if ENABLE_IL2CPP
     /// <summary>
     /// Add an empty target to an ability
     /// </summary>
-    [Serializable]
-#if ENABLE_IL2CPP
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
 #endif
+    [Serializable]
     public class InstantDestroySystem : IEcsInitSystem, IEcsRunSystem
     {
         private EcsWorld _world;
