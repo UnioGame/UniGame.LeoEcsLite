70ecca754d5b032ea4758c2cae28394a347cd56e
Mefodei <mefodei.link@gmail.com>
2023-06-30 09:18:06 +0700

    - add common light component

---
+++ LeoEcs.Shared/Components/LightComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace Game.Modules.UnioModules.UniGame.LeoEcsLite.LeoEcs.Shared.Components
+{
+    using System;
+    using UnityEngine;
+
+    /// <summary>
+    /// unity light component
+    /// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+    [Serializable]
+    public struct LightComponent
+    {
+        public Light Value;
+    }
+}
