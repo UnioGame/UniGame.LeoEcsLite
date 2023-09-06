1ccbba33d32d93f574967d9691e9526cf6e901da
Mefodei <mefodei.link@gmail.com>
2023-09-07 03:49:55 +0700

    - add resource component

---
+++ LeoEcs.Shared/Components/ResourceComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace UniGame.LeoEcs.Shared.Components
+{
+    using System;
+
+    /// <summary>
+    /// resource id component
+    /// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+    [Serializable]
+    public struct ResourceComponent
+    {
+        public string Value;
+    }
+}
