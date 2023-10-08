cbf1c8acb627c6304d780d9900f91d391898c072
Mefodei <mefodei.link@gmail.com>
2023-10-08 16:01:38 +0700

    - add world globals

---
+++ LeoEcs.Shared/Extensions/WorldGlobalExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace UniGame.LeoEcs.Shared.Extensions
+{
+    using Leopotam.EcsLite;
+    using UniModules.UniCore.Runtime.Utils;
+    using UniModules.UniGame.Context.Runtime.Context;
+
+    public static class WorldGlobalExtensions
+    {
+        private static MemorizeItem<EcsWorld, EntityContext> _globalValues = MemorizeTool
+            .Memorize<EcsWorld, EntityContext>(x =>
+            {
+                var context = new EntityContext();
+                var worldLifeTime = x.GetWorldLifeTime();
+                context.AddTo(worldLifeTime);
+                return context;
+            });
+
+        public static T GetGlobal<T>(this EcsWorld world)
+        {
+            var globals = _globalValues[world];
+            var value = globals.Get<T>();
+            return value;
+        }
+        
+        public static T SetGlobal<T>(this EcsWorld world,T value)
+        {
+            var globals = _globalValues[world];
+            globals.Publish(value);
+            return value;
+        }
+    }
+}
