1ce3219ff50f6b6648816d184c6320d179a3c5fa
Mefodei <mefodei.link@gmail.com>
2023-11-23 04:36:25 +0700

    - remove view t component

--- LeoEcs.ViewSystem/Components/ViewComponent.cs
+++ LeoEcs.ViewSystem/Components/ViewComponent.cs
@@ -3,18 +3,10 @@ using UniGame.ViewSystem.Runtime;
 
 namespace UniGame.LeoEcs.ViewSystem.Components
 {
-    using Converters;
-
     [Serializable]
     public struct ViewComponent
     {
         public IView View;
         public Type Type;
-    }
-
-    [Serializable]
-    public struct ViewComponent<TView>
-    {
-        public TView View;
     }
 }
