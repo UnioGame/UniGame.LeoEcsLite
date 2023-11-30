547ebaef053740aabbe6da47b866bc3bbcade8ad
Mefodei <mefodei.link@gmail.com>
2023-11-30 21:03:40 +0700

    - remove rotation from base converter

--- LeoEcs.Converter/Runtime/Converters/BaseGameObjectComponentConverter.cs
+++ LeoEcs.Converter/Runtime/Converters/BaseGameObjectComponentConverter.cs
@@ -17,26 +17,13 @@ namespace UniGame.LeoEcs.Converter.Runtime.Converters
             ref var transformComponent = ref world.GetOrAddComponent<TransformComponent>(entity);
             ref var gameObjectComponent = ref world.GetOrAddComponent<GameObjectComponent>(entity);
             ref var objectComponent = ref world.GetOrAddComponent<UnityObjectComponent>(entity);
-
             ref var transformPositionComponent = ref world.GetOrAddComponent<TransformPositionComponent>(entity);
-            ref var directionComponent = ref world.GetOrAddComponent<TransformDirectionComponent>(entity);
-            ref var scaleComponent = ref world.GetOrAddComponent<TransformScaleComponent>(entity);
-            ref var rotationComponent = ref world.GetOrAddComponent<TransformRotationComponent>(entity);
 
             var transform = target.transform;
             transformComponent.Value = transform;
             gameObjectComponent.Value = target;
             objectComponent.Value = target;
             transformPositionComponent.Position = transform.position;
-            directionComponent.Forward = transform.forward;
-            directionComponent.Up = transform.up;
-            directionComponent.Right = transform.right;
-            scaleComponent.Scale = transform.lossyScale;
-            scaleComponent.LocalScale = transform.localScale;
-
-            rotationComponent.Quaternion = transform.rotation;
-            rotationComponent.LocalRotation = transform.localRotation;
-            rotationComponent.Euler = transform.eulerAngles;
         }
     }
 }
