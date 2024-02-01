8b77ec375e25adcccd155bcd97904d661513ced0
Mefodei <mefodei.link@gmail.com>
2024-02-01 17:00:45 +0700

    - add hashcode component

---
+++ LeoEcs.Shared/Components/HashComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace UniGame.LeoEcsLite.LeoEcs.Shared.Components
+{
+    using System;
+
+    /// <summary>
+    /// hash value component
+    /// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+    [Serializable]
+    public struct HashComponent
+    {
+        public int Value;
+    }
+}
