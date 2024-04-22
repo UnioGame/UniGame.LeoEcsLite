6c594b77d0b993e45c79ced94a41b6787cf9f24f
mefodei <mefodei.link@gmail.com>
2024-04-22 23:44:44 +0700

    - update shared converters

---
+++ LeoEcs.Shared/Core/Converters/RigidbodyConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace Game.Ecs.GameAi.MoveToTarget.Converters
+{
+	using System;
+	using System.Threading;
+	using Leopotam.EcsLite;
+	using UniCore.Runtime.ProfilerTools;
+	using UniGame.LeoEcs.Converter.Runtime;
+	using UniGame.LeoEcs.Shared.Components;
+	using UniGame.LeoEcs.Shared.Extensions;
+	using UnityEngine;
+
+	/// <summary>
+	/// Add Rigidbody Component to entity
+	/// </summary>
+#if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+#endif
+	[Serializable]
+	public class RigidbodyConverter : LeoEcsConverter
+	{
+		public override void Apply(GameObject target, EcsWorld world, int entity)
+		{
+			var rigidbody = target.GetComponent<Rigidbody>();
+#if UNITY_EDITOR
+			if (!rigidbody)
+			{
+				GameLog.Log($"{nameof(RigidbodyConverter)} WRONG TARGET",Color.red);
+				return;
+			}
+#endif
+			ref var rigidbodyComponent = ref world.GetOrAddComponent<RigidbodyComponent>(entity);
+			rigidbodyComponent.Value = rigidbody;
+		}
+	}
+}
